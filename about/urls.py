3545e9483bd995409446c5b444b77de29b3122fb
Atami <takahiroatsumi0517@gmail.com>
2016-02-09 04:58:11 +0900

    Created base contact form.
    Created simple contact form.
    ready to captcha by comment out.

--- about/urls.py
+++ about/urls.py
@@ -4,12 +4,18 @@ r"""urls -- DESCRIPTION
 
 """
 from django.conf.urls import patterns, url
-from about.group import views
+# from about.group.views import groupview
+# from about.contact.views import contactform
+
 
 urlpatterns = patterns(
     '',
     # url(r'^$', views.index, name='index'),
-    url(r'^group/', views.group, name='group')
+    # url(r'^group/', groupview, name='group'),
+    (r'^group/', 'about.group.views.groupview'),
+    (r'^contact/thankyou/', 'about.contact.views.thankyou'),
+    (r'^contact/', 'about.contact.views.contactform'),
+
 )
 
 

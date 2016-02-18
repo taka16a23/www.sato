9c6ad2204576e8e5fc25a2d1333b74b94e374c82
Atami <takahiroatsumi0517@gmail.com>
2016-02-18 23:37:09 +0900

    moved contact and group.
    moved contact and group to about

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from about.models import ContactPostModel
+
+
+@admin.register(ContactPostModel)
+class ContactAdmin(admin.ModelAdmin):
+    r"""ContactAdmin
+
+    ContactAdmin is a admin.ModelAdmin.
+    Responsibility:
+    """
+    list_display = ('name', 'title', 'created', 'email', 'body',  'finished')
+    list_editable = ('finished', )
+    # radio_fields = {"finished": admin.HORIZONTAL}
+
+
+
+# For Emacs
+# Local Variables:
+# coding: utf-8
+# End:
+# admin.py ends here
--- about/views.py
+++ about/views.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 from lib.utils import get_context
+
+from about.forms import ContactPostForm
 
 
 def about_view(request):
@@ -21,6 +24,50 @@ def about_view(request):
     context = get_context()
     return render_to_response(
         'about/index.html', context, context_instance=RequestContext(request))
+
+
+def group_view(request):
+    r"""SUMMARY
+
+    group_view(request)
+
+    @Arguments:
+    - `request`:
+
+    @Return:
+
+    @Error:
+    """
+    context = get_context()
+    return render_to_response(
+        'group/index.html', context, context_instance=RequestContext(request))
+
+
+def contactform(request):
+    context = get_context()
+    form = ContactPostForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/contact/thankyou/')
+    context['form'] = ContactPostForm()
+    return render_to_response(
+        'contact/index.html',
+        context,
+        context_instance=RequestContext(request))
+
+def thankyou(request):
+    r"""SUMMARY
+
+    thankyou(request)
+
+    @Arguments:
+    - `request`:
+
+    @Return:
+
+    @Error:
+    """
+    return render_to_response('contact/thankyou.html', )
 
 
 

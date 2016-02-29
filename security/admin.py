c7312ae36b9bb24fe30a3ec8dc5c7a87bd7cacee
Atami <takahiroatsumi0517@gmail.com>
2016-02-29 15:20:04 +0900

    Moved emergency to security.

--- security/admin.py
+++ security/admin.py
@@ -2,8 +2,42 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from sato.settings import STATIC_URL
-from security.models import SecKnowledgeModel
+from security.models import SecKnowledgeModel, EmergencyEntryModel
+
+def make_unpublish(modeladmin, request, queryset):
+    r"""SUMMARY
+
+    unpublish_all(modeladmin, request, queryset)
+
+    @Arguments:
+    - `modeladmin`:
+    - `request`:
+    - `queryset`:
+
+    @Return:
+
+    @Error:
+    """
+    queryset.update(status=2)
+
+
+make_unpublish.short_description = u'非公開にする'
+
+
+@admin.register(EmergencyEntryModel)
+class EmergencyEntryAdmin(admin.ModelAdmin):
+    r"""EmergencyEntryAdmin
+
+    EmergencyEntryAdmin is a admin.ModelAdmin.
+    Responsibility:
+    """
+    list_display = ('title', 'status', 'publish_date', 'expiry_date', 'description')
+    list_editable = ('status', )
+    ordering = ('-modified', )
+    exclude = ('description', )
+    radio_fields = {"status": admin.HORIZONTAL}
+    actions = [make_unpublish, 'delete_selected']
+
 
 @admin.register(SecKnowledgeModel)
 class SecKnowledgeAdmin(admin.ModelAdmin):

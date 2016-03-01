946390749fca3f2f557e98971d03b0f44ea21919
Atami <takahiroatsumi0517@gmail.com>
2016-03-02 00:00:24 +0900

    apps separated to subdirectory.

---
+++ formats/admin/sato.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""sato -- DESCRIPTION
+
+"""
+from django.contrib import admin
+
+from formats.models import SatoFormat
+from adminsortable2.admin import SortableAdminMixin
+
+@admin.register(SatoFormat)
+class SatoFormatAdmin(SortableAdminMixin, admin.ModelAdmin):
+    r"""SatoFormatAdmin
+
+    SatoFormatAdmin is a admin.ModelAdmin.
+    Responsibility:
+    """
+    list_display = ('title', 'short_description', 'file', 'form', 'publish')
+    list_editable = ('publish', )
+    exclude = ['sortid', 'short_description', ]
+
+
+
+# For Emacs
+# Local Variables:
+# coding: utf-8
+# End:
+# sato.py ends here
---
+++ formats/admin/staff.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""staff -- DESCRIPTION
+
+"""
+from django.contrib import admin
+
+from formats.models import StaffFormat
+from adminsortable2.admin import SortableAdminMixin
+
+
+@admin.register(StaffFormat)
+class StaffFormatAdmin(SortableAdminMixin, admin.ModelAdmin):
+    r"""StaffFormatAdmin
+
+    OtherFormatAdmin is a admin.ModelAdmin.
+    Responsibility:
+    """
+    list_display = ('title', 'short_description', 'file', )
+    exclude = ['sortid', 'short_description', ]
+
+
+
+# For Emacs
+# Local Variables:
+# coding: utf-8
+# End:
+# staff.py ends here

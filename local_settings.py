36a0a20b3f3ccae165265c4e75502abae00d7ffa
Atami <takahiroatsumi0517@gmail.com>
2016-03-02 03:22:32 +0900

    deleted

--- local_settings.py
+++
@@ -1,14 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-r"""local_settings -- DESCRIPTION
-
-"""
-SITE_URL = ''
-
-
-
-# For Emacs
-# Local Variables:
-# coding: utf-8
-# End:
-# local_settings.py ends here

49e77b16309cb167702b023662cec30628830e00
Atami <takahiroatsumi0517@gmail.com>
2016-03-09 00:26:16 +0900

    added ckeditor static

---
+++ base/static/ckeditor/ckeditor/plugins/slideshow/lang/ja.js
@@ -0,0 +1,41 @@
+﻿/*
+Copyright (c) 2003-2013, Cricri042. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+Targeted for "ad-gallery" JavaScript : http://adgallery.codeplex.com/
+And "Fancybox" : http://fancyapps.com/fancybox/
+*/
+CKEDITOR.plugins.setLang( 'slideshow', 'ja', {
+	toolbar	: 'スライドショー',
+	dialogTitle : 'スライドショー プロパティー',
+	fakeObject : 'スライドショー',
+	imgList : '画像',
+	imgAdd : '画像を追加',
+	imgTitle : 'タイトル',
+	imgDelete : '削除',
+	imgEdit : '編集',
+  validModif : '保存',
+	previewMode : 'プレビュー',
+	imgDesc : '概要',
+	editSlideShow : 'スライドショーを編集',
+	picturesList : "画像リスト :",
+	insertSlideShow : 'Insert SlideShow',
+	showThumbs : 'サムネイル表示',
+	showTitle : 'タイトル表示',
+	showControls : "Show Start / Stop",
+	labelStart : "Start",
+	labelStop : "Stop",
+	arrowUp : "\u2191",
+	arrowDown : "\u2193",
+	displayTime : 'Disp. Time (Sec.)',
+	transitionTime : 'Tr. Time (mSec.)',
+	autoStart : 'AutoStart',
+	pictHeight : '高さ (px)',
+	pictWidth : '幅 (px)',
+	openOnClick : 'Open on Click',
+	transition : 'Transition Type',
+	tr1 : 'なし',
+	tr2 : 'リサイズ',
+	tr3 : '縦スライド',
+	tr4 : '横スライド',
+	tr5 : 'フェードアウト',
+});

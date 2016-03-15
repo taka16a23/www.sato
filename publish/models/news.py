91553386ba8eff95561387e199fe3ef5df6cf426
Atami <takahiroatsumi0517@gmail.com>
2016-03-16 03:28:37 +0900

    Changed method

--- publish/models/news.py
+++ publish/models/news.py
@@ -11,36 +11,6 @@ from core.models import DisplayableModel
 from core.managers import DisplayableQuerySet, ManagerAbstract
 from colorfield.fields import ColorField
 
-NEWS_IMPORTANT = 1
-NEWS_INFORMATION = 2
-NEWS_SECURITY = 3
-NEWS_ACTIVITY = 4
-NEWS_WANTED = 5
-NEWS_UPDATE = 6
-
-
-NEWS_CATEGORIES = (
-    (NEWS_IMPORTANT, u'重要'),
-    (NEWS_INFORMATION, u'お知らせ'),
-    (NEWS_SECURITY, u'防犯'),
-    (NEWS_ACTIVITY, u'ご報告'),
-    (NEWS_WANTED, u'募集'),
-    (NEWS_UPDATE, u'更新'),
-)
-
-NEWS_CATEGORIES_DIC = {integer: name for integer, name in NEWS_CATEGORIES}
-
-CATEGORY_CLASS_KEYS = {
-    NEWS_IMPORTANT: 'categoryImportant',
-    NEWS_INFORMATION: 'categoryInfo',
-    NEWS_SECURITY: 'categorySecurity',
-    NEWS_ACTIVITY: 'categoryReport',
-    NEWS_WANTED: 'categoryWanted',
-    NEWS_UPDATE: 'categoryUpdate',
-}
-
-MIN_NEWS_COUNTS = 8
-
 
 class NewsCategoryModel(models.Model):
     r"""NewsCategoryModel
@@ -73,6 +43,9 @@ class NewsCategoryModel(models.Model):
         return self.name
 
 
+RECENT_DAYS = 7 # 最低表示する期間はここを編集(日数)
+MIN_NEWS_COUNTS = 8 # 最低表示する数はここを編集(個数)
+
 
 class NewsPostQuerySet(DisplayableQuerySet):
     r"""NewsPostQuerySet
@@ -80,19 +53,24 @@ class NewsPostQuerySet(DisplayableQuerySet):
     NewsPostQuerySet is a DisplayableQuerySet.
     Responsibility:
     """
-    def latest_by_days(self, days, min_news_counts=MIN_NEWS_COUNTS):
+    def list_recently(self, recentdays=RECENT_DAYS, min_news_counts=MIN_NEWS_COUNTS):
         r"""SUMMARY
 
-        name()
+        @Arguments:
+        - `recentdays`:
+        - `min_news_counts`: news 最低個数
 
         @Return:
+
+        name()
 
         @Error:
         """
         end = now = datetime.datetime.now()
-        start = now - datetime.timedelta(days)
+        start = now - datetime.timedelta(recentdays)
         news = self.published().order_by('-publish_date')[:min_news_counts]
-        rangenews = self.range_by_publish_date(start, end).order_by('-publish_date')
+        rangenews = self.published().range_by_publish_date(
+            start, end).order_by('-publish_date')
         if len(news) < len(rangenews):
             return rangenews
         return news
@@ -125,37 +103,16 @@ class NewsPostModel(DisplayableModel):
     objects = NewsPostManager()
 
     title = models.CharField(u'タイトル', max_length=200, blank=False)
-    category = models.ForeignKey(NewsCategoryModel, null=False)
+    category = models.ForeignKey(
+        NewsCategoryModel, verbose_name=u'カテゴリー', null=False)
     url = models.URLField('URL', max_length=200, blank=False)
 
-    class Meta:
+    class Meta(object):
         verbose_name = u'お知らせ'
         verbose_name_plural = u'お知らせ'
 
     def __unicode__(self):
         return unicode(self.title)
-
-    def as_class_key(self, ):
-        r"""SUMMARY
-
-        as_class_key()
-
-        @Return:
-
-        @Error:
-        """
-        return CATEGORY_CLASS_KEYS.get(self.category)
-
-    def as_category_name(self, ):
-        r"""SUMMARY
-
-        as_category_name()
-
-        @Return:
-
-        @Error:
-        """
-        return NEWS_CATEGORIES_DIC.get(self.category, u'')
 
 
 
